11ebba6831e4a5752ba0cc5d556f7928f001d88e
Chris Troutner <chris.troutner@gmail.com>
2021-08-04 16:26:39 -0700

    Got core functionality of wallet-update command working

--- src/lib/wallet-util.js
+++ src/lib/wallet-util.js
@@ -3,20 +3,66 @@
 */
 
 const fs = require('fs').promises
+const BCHJS = require('@psf/bch-js')
 
 let _this // Global variable points at instance of this Class.
 
 class WalletUtil {
-  constructor (localConfig = {}) {
+  constructor(localConfig = {}) {
     this.fs = fs
+    this.bchjs = new BCHJS()
 
     _this = this
   }
 
-  async saveWallet (filename, walletData) {
+  // Save the wallet data into a .json text file.
+  async saveWallet(filename, walletData) {
     await _this.fs.writeFile(filename, JSON.stringify(walletData, null, 2))
 
     return true
+  }
+
+  // Generates an array of HD addresses.
+  // Address are generated from index to limit.
+  // e.g. generateAddress(walletData, 20, 10)
+  // will generate a 10-element array of addresses from index 20 to 29
+  async generateAddress(walletData, index, limit) {
+    // console.log(`walletData: ${JSON.stringify(walletData, null, 2)}`)
+
+    if (!walletData.mnemonic) throw new Error('mnemonic is undefined!')
+
+    // root seed buffer
+    const rootSeed = await this.bchjs.Mnemonic.toSeed(walletData.mnemonic)
+
+    // master HDNode
+    let masterHDNode
+    if (walletData.network === 'testnet') {
+      masterHDNode = this.bchjs.HDNode.fromSeed(rootSeed, 'testnet')
+    } else masterHDNode = this.bchjs.HDNode.fromSeed(rootSeed)
+
+    // HDNode of BIP44 account
+    const account = this.bchjs.HDNode.derivePath(
+      masterHDNode,
+      `m/44'/${walletData.derivation}'/0'`,
+    )
+
+    // Empty array for collecting generated addresses
+    const bulkAddresses = []
+
+    // Generate the addresses.
+    for (let i = index; i < index + limit; i++) {
+      // derive an external change address HDNode
+      const change = this.bchjs.HDNode.derivePath(account, `0/${i}`)
+
+      // get the cash address
+      const newAddress = this.bchjs.HDNode.toCashAddress(change)
+      // const legacy = this.bchjs.HDNode.toLegacyAddress(change)
+
+      // push address into array
+      bulkAddresses.push(newAddress)
+    }
+
+    return bulkAddresses
   }
 }
 
--- test/mocks/wallet-util-mock.js
+++ test/mocks/wallet-util-mock.js
@@ -12,8 +12,8 @@ const mockSpentUtxo = [
     height: 1332533,
     confirmations: 32,
     satoshis: 1000,
-    hdIndex: 1
-  }
+    hdIndex: 1,
+  },
 ]
 
 const mockUnspentUtxo = [
@@ -23,62 +23,93 @@ const mockUnspentUtxo = [
     amount: 0.00006,
     satoshis: 6000,
     height: 603562,
-    confirmations: 10
-  }
+    confirmations: 10,
+  },
 ]
 
 const mockWallet = {
-  network: 'testnet',
   mnemonic:
-    'alert mad wreck salon target later across crater stick mammal grunt ability',
+    'wool coil panel mammal victory view enjoy dream dynamic cycle pig help',
   derivation: 245,
-  rootAddress: 'bchtest:qzl37uzel5urphw8dnkerxtlr5mxunvsys062p7g9v',
-  balance: 0.00001,
+  rootAddress: 'bitcoincash:qrqug96lmh6mvd43gzld0vgz6mkk3vktgvpfzdrqkm',
+  balance: 0.00009636,
   nextAddress: 3,
   hasBalance: [
     {
       index: 1,
-      balance: 0.00001,
-      balanceSat: 1000,
-      unconfirmedBalance: 0,
-      unconfirmedBalanceSat: 0,
-      cashAddress: 'bchtest:qzylf04c9f9d20gndgw9dp82602umwzuuqhnxv7jmn'
-    }
-  ],
-  name: 'temp',
-  balanceConfirmed: 0.00001,
-  balanceUnconfirmed: 0,
-  SLPUtxos: [],
-  addresses: [
-    [0, 'bchtest:qzl37uzel5urphw8dnkerxtlr5mxunvsys062p7g9v'],
-    [1, 'bchtest:qzylf04c9f9d20gndgw9dp82602umwzuuqhnxv7jmn'],
-    [2, 'bchtest:qp6dyeslwkslzruaf29vvtv6lg7lez8csca90lg6a0'],
-    [3, 'bchtest:qqkng037s5pjhhk38mkaa3c6grl3uep845evtxvyse']
-  ]
-}
-
-const mainnetWallet = {
-  network: 'mainnet',
-  mnemonic:
-    'shaft jelly okay little immense grant poet east there urge exhaust swarm',
-  derivation: 245,
-  rootAddress: 'bitcoincash:qr7rzs9jmng7y2j0v9dh3acr08p54hhw9vxhne9dew',
-  balance: 0.00001,
-  nextAddress: 2,
-  hasBalance: [
+      balance: 0,
+      balanceSat: 0,
+      unconfirmedBalance: 0.0000909,
+      unconfirmedBalanceSat: 9090,
+      cashAddress: 'bitcoincash:qquwktj59rv6feg7kmaa9ynkpgeqhcledynsa6ycxh',
+    },
     {
-      index: 1,
-      balance: 0.00001,
-      balanceSat: 1000,
-      unconfirmedBalance: 0,
-      unconfirmedBalanceSat: 0,
-      cashAddress: 'bitcoincash:qzxj70q8xkw0sjkj526uc4k08g36sldldygxe7pgn4'
-    }
+      index: 2,
+      balance: 0,
+      balanceSat: 0,
+      unconfirmedBalance: 0.00000546,
+      unconfirmedBalanceSat: 546,
+      cashAddress: 'bitcoincash:qr7ku4y5hy92sfsq9le0ula3rx5n33l8u5qz8mmy55',
+    },
   ],
+  addresses: [
+    [0, 'bitcoincash:qrqug96lmh6mvd43gzld0vgz6mkk3vktgvpfzdrqkm'],
+    [1, 'bitcoincash:qquwktj59rv6feg7kmaa9ynkpgeqhcledynsa6ycxh'],
+    [2, 'bitcoincash:qr7ku4y5hy92sfsq9le0ula3rx5n33l8u5qz8mmy55'],
+  ],
+  description: '',
   name: 'temp',
-  balanceConfirmed: 0.00001,
-  balanceUnconfirmed: 0,
-  SLPUtxos: []
+  balanceConfirmed: 0,
+  balanceUnconfirmed: 0.00009636,
+  SLPUtxos: [
+    {
+      address: 'bitcoincash:qr7ku4y5hy92sfsq9le0ula3rx5n33l8u5qz8mmy55',
+      utxos: [
+        {
+          height: 0,
+          tx_hash:
+            '7a402444111750e744b1f4f9fdcf2050385312d8422c6683d20f752565384dee',
+          tx_pos: 1,
+          value: 546,
+          txid: '7a402444111750e744b1f4f9fdcf2050385312d8422c6683d20f752565384dee',
+          vout: 1,
+          utxoType: 'token',
+          transactionType: 'send',
+          tokenId:
+            'a4fb5c2da1aa064e25018a43f9165040071d9e984ba190c222a7f59053af84b2',
+          tokenTicker: 'TROUT',
+          tokenName: "Trout's test token",
+          tokenDocumentUrl: 'troutsblog.com',
+          tokenDocumentHash: '',
+          decimals: 2,
+          tokenType: 1,
+          isValid: true,
+          tokenQty: '1.2',
+          address: 'bitcoincash:qr7ku4y5hy92sfsq9le0ula3rx5n33l8u5qz8mmy55',
+          hdIndex: 2,
+        },
+      ],
+    },
+  ],
+  BCHUtxos: [
+    {
+      address: 'bitcoincash:qquwktj59rv6feg7kmaa9ynkpgeqhcledynsa6ycxh',
+      utxos: [
+        {
+          height: 0,
+          tx_hash:
+            '17b1ef0a9995e621fd5dfc4e44adc0a07b62d2d0952ced92ccbcb1043a14cf13',
+          tx_pos: 0,
+          value: 9090,
+          txid: '17b1ef0a9995e621fd5dfc4e44adc0a07b62d2d0952ced92ccbcb1043a14cf13',
+          vout: 0,
+          isValid: false,
+          address: 'bitcoincash:qquwktj59rv6feg7kmaa9ynkpgeqhcledynsa6ycxh',
+          hdIndex: 1,
+        },
+      ],
+    },
+  ],
 }
 
 const mockTxOut = {
@@ -86,20 +117,18 @@ const mockTxOut = {
   confirmations: 11,
   value: 0.00006,
   scriptPubKey: {
-    asm:
-      'OP_DUP OP_HASH160 d19fae66b685f5c3633c0db0600313918347225f OP_EQUALVERIFY OP_CHECKSIG',
+    asm: 'OP_DUP OP_HASH160 d19fae66b685f5c3633c0db0600313918347225f OP_EQUALVERIFY OP_CHECKSIG',
     hex: '76a914d19fae66b685f5c3633c0db0600313918347225f88ac',
     reqSigs: 1,
     type: 'pubkeyhash',
-    addresses: ['bitcoincash:qrgeltnxk6zltsmr8sxmqcqrzwgcx3eztusrwgf0x3']
+    addresses: ['bitcoincash:qrgeltnxk6zltsmr8sxmqcqrzwgcx3eztusrwgf0x3'],
   },
-  coinbase: false
+  coinbase: false,
 }
 
 module.exports = {
   mockSpentUtxo,
   mockUnspentUtxo,
   mockWallet,
-  mainnetWallet,
-  mockTxOut
+  mockTxOut,
 }
