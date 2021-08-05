c295b06e9999802fa83522b76326cde56b7714d8
Chris Troutner <chris.troutner@gmail.com>
2021-08-04 21:07:14 -0700

    feat(wallet-update): Added command to update wallet balances

--- src/commands/wallet-list.js
+++ src/commands/wallet-list.js
@@ -7,17 +7,17 @@
 const shelljs = require('shelljs')
 const Table = require('cli-table')
 
-const { Command } = require('@oclif/command')
+const {Command} = require('@oclif/command')
 
 class WalletList extends Command {
-  constructor (argv, config) {
+  constructor(argv, config) {
     super(argv, config)
 
     // Encapsulate dependencies.
     this.shelljs = shelljs
   }
 
-  async run () {
+  async run() {
     const walletData = this.parseWallets()
     // console.log(`walletData: ${JSON.stringify(walletData, null, 2)}`)
 
@@ -25,9 +25,9 @@ class WalletList extends Command {
   }
 
   // Parse data from the wallets directory into a formatted array.
-  parseWallets () {
+  parseWallets() {
     const fileList = this.shelljs.ls(
-      `${__dirname.toString()}/../../.wallets/*.json`
+      `${__dirname.toString()}/../../.wallets/*.json`,
     )
     // console.log('fileList: ', fileList)
 
@@ -45,7 +45,7 @@ class WalletList extends Command {
 
       const lastPart = thisFile.indexOf('.json')
 
-      const lastSlash = thisFile.indexOf('.wallets/')
+      const lastSlash = thisFile.indexOf('.wallets/') + 1
       // console.log(`lastSlash: ${lastSlash}`)
 
       let name = thisFile.slice(8, lastPart)
@@ -65,10 +65,10 @@ class WalletList extends Command {
   }
 
   // Display table in a table on the command line using cli-table.
-  displayTable (data) {
+  displayTable(data) {
     const table = new Table({
       head: ['Name', 'Balance (BCH)'],
-      colWidths: [25, 15]
+      colWidths: [25, 15],
     })
 
     for (let i = 0; i < data.length; i++) table.push(data[i])
