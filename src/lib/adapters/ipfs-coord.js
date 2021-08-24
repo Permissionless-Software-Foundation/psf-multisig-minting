0aab9d849bf6d86372fa8d2a62dcf2c9b55d6ba4
Chris Troutner <chris.troutner@gmail.com>
2021-08-24 14:00:34 -0700

    fix(ipfs-coord): Updated to v6.1.5

--- src/lib/adapters/ipfs-coord.js
+++ src/lib/adapters/ipfs-coord.js
@@ -10,30 +10,30 @@ const semver = require('semver')
 const Conf = require('conf')
 
 // The minimum version of ipfs-bch-wallet-service that this wallet can work with.
-const MIN_BCH_WALLET_VERSION = '1.9.0'
+const MIN_BCH_WALLET_VERSION = '1.11.0'
 const WALLET_PROTOCOL = 'bch-wallet'
 
 let _this
 
 class IpfsCoordAdapter {
-  constructor (localConfig = {}) {
+  constructor(localConfig = {}) {
     // Dependency injection.
     this.ipfs = localConfig.ipfs
     if (!this.ipfs) {
       throw new Error(
-        'Instance of IPFS must be passed when instantiating ipfs-coord adapter.'
+        'Instance of IPFS must be passed when instantiating ipfs-coord adapter.',
       )
     }
     this.bchjs = localConfig.bchjs
     if (!this.bchjs) {
       throw new Error(
-        'Instance of bch-js must be passed when instantiating ipfs-coord adapter.'
+        'Instance of bch-js must be passed when instantiating ipfs-coord adapter.',
       )
     }
     this.eventEmitter = localConfig.eventEmitter
     if (!this.eventEmitter) {
       throw new Error(
-        'An instance of an EventEmitter must be passed when instantiating the ipfs-coord adapter.'
+        'An instance of an EventEmitter must be passed when instantiating the ipfs-coord adapter.',
       )
     }
 
@@ -54,14 +54,14 @@ class IpfsCoordAdapter {
     // State object. TODO: Make this more robust.
     this.state = {
       serviceProviders: [],
-      selectedServiceProvider: ''
+      selectedServiceProvider: '',
     }
 
     _this = this
   }
 
   // Start the IPFS node.
-  async start (localConfig = {}) {
+  async start(localConfig = {}) {
     this.ipfsCoord = new this.IpfsCoord({
       ipfs: this.ipfs,
       type: 'node.js',
@@ -70,11 +70,12 @@ class IpfsCoordAdapter {
       privateLog: this.peerInputHandler, // Default to console.log
       isCircuitRelay: false,
       apiInfo: '',
-      announceJsonLd: announceJsonLd
+      announceJsonLd: announceJsonLd,
+      debugLevel: 2,
     })
 
     // Wait for the ipfs-coord library to signal that it is ready.
-    await this.ipfsCoord.ipfs.start()
+    await this.ipfsCoord.start()
     // await this.ipfsCoord.isReady()
 
     // Signal that this adapter is ready.
@@ -96,7 +97,7 @@ class IpfsCoordAdapter {
 
   // Expects router to be a function, which handles the input data from the
   // pubsub channel. It's expected to be capable of routing JSON RPC commands.
-  attachRPCRouter (router) {
+  attachRPCRouter(router) {
     try {
       _this.ipfsCoord.privateLog = router
       _this.ipfsCoord.ipfs.orbitdb.privateLog = router
@@ -107,24 +108,25 @@ class IpfsCoordAdapter {
   }
 
   // Poll the ipfs-coord coordination channel for available service providers.
-  pollForServices () {
+  pollForServices() {
     try {
       // An array of IPFS IDs of other nodes in the coordination pubsub channel.
-      const peers = _this.ipfsCoord.ipfs.peers.state.peerList
+      const peers = _this.ipfsCoord.thisNode.peerList
       // console.log(`peers: ${JSON.stringify(peers, null, 2)}`)
 
       // Array of objects. Each object is the IPFS ID of the peer and contains
       // data about that peer.
-      const peerData = _this.ipfsCoord.ipfs.peers.state.peers
+      const peerData = _this.ipfsCoord.thisNode.peerData
       // console.log(`peerData: ${JSON.stringify(peerData, null, 2)}`)
 
       for (let i = 0; i < peers.length; i++) {
         const thisPeer = peers[i]
-        const thisPeerData = peerData[thisPeer]
+        const thisData = peerData.filter(x => x.from === thisPeer)
+        const thisPeerData = thisData[0]
 
         // Create a 'fingerprint' that defines the wallet service.
-        const protocol = thisPeerData.jsonLd.protocol
-        const version = thisPeerData.jsonLd.version
+        const protocol = thisPeerData.data.jsonLd.protocol
+        const version = thisPeerData.data.jsonLd.version
 
         let versionMatches = false
         if (version) {
@@ -134,7 +136,7 @@ class IpfsCoordAdapter {
         // Ignore any peers that don't match the fingerprint for a BCH wallet
         // service.
         if (protocol && protocol.includes(WALLET_PROTOCOL) && versionMatches) {
-          // console.log('Matching peer: ', thisPeerData)
+          console.log('Matching peer: ', thisPeerData)
 
           // Temporary business logic.
           // Use the first available wallet service detected.
@@ -158,7 +160,7 @@ class IpfsCoordAdapter {
 
   // This method handles input coming in from other IPFS peers.
   // It passes the data on to the REST API library by emitting an event.
-  peerInputHandler (data) {
+  peerInputHandler(data) {
     try {
       // console.log('peerInputHandler triggered with this data: ', data)
 
@@ -177,7 +179,7 @@ const announceJsonLd = {
   '@context': 'https://schema.org/',
   '@type': 'Person',
   name: `wallet-consumer-${randNum}`,
-  description: 'A consumer of BCH wallet services'
+  description: 'A consumer of BCH wallet services',
 }
 
 module.exports = IpfsCoordAdapter
