99846fbb6bbaa364092f522e3bf653a672059f72
Daniel Gonzalez <danielhumgon@gmail.com>
2021-12-30 22:08:03 -0400

    Finished unit tests

--- src/commands/msg-send.js
+++ src/commands/msg-send.js
@@ -1,20 +1,15 @@
 const WalletService = require('../lib/adapters/wallet-consumer')
 
 const { Command, flags } = require('@oclif/command')
-const EncryptLib = require('bch-encrypt-lib')
+const EncryptLib = require('bch-encrypt-lib/index')
 const MessagesLib = require('bch-message-lib')
 
 const eccrypto = require('eccrypto-js')
 const Write = require('p2wdb/index').Write
-// const Conf = require('conf')
 
 class MsgSend extends Command {
   constructor (argv, config) {
     super(argv, config)
-    // Encapsulate dependencies.
-    // this.conf = new Conf()
-    // this.restServer = this.conf.get('restServer')
-    // console.log(`restServer: ${this.restServer}`)
 
     this.walletService = new WalletService()
     this.encryptLib = new EncryptLib({ bchjs: this.walletService.walletUtil.bchjs })
@@ -29,8 +24,10 @@ class MsgSend extends Command {
 
       // Validate input flags
       this.validateFlags(flags)
+      const filename = `${__dirname.toString()}/../../.wallets/${flags.name
+        }.json`
 
-      const result = await this.msgSend(flags)
+      const result = await this.msgSend(filename, flags)
 
       return result
     } catch (error) {
@@ -40,11 +37,14 @@ class MsgSend extends Command {
     }
   }
 
-  async msgSend ({ bchAddress, message, name, subject }) {
+  async msgSend (filename, flags) {
     try {
-      const filename = `${__dirname.toString()}/../../.wallets/${name
-        }.json`
+      // Input validation
+      if (!filename || typeof filename !== 'string') {
+        throw new Error('filename is required.')
+      }
 
+      const { bchAddress, message, subject } = flags
       // Load the wallet file.
       const walletJSON = require(filename)
       const walletData = walletJSON.wallet
@@ -53,14 +53,13 @@ class MsgSend extends Command {
       const p2wdbConfig = {
         wif: walletData.privateKey
       }
-      const write = new this.Write(p2wdbConfig)
+      this.write = new this.Write(p2wdbConfig)
 
       // Get public Key
       const pubKey = await this.walletService.getPubKey(bchAddress)
       const publicKey = pubKey.pubkey.publicKey
       console.log(`publicKey: ${JSON.stringify(publicKey, null, 2)}`)
 
-      // const encryptedMsg = await this.bchEncryptMsg(publicKey, message)
       const encryptedMsg = await this.encryptMsg(publicKey, message)
       console.log(`encryptedMsg: ${JSON.stringify(encryptedMsg, null, 2)}`)
 
@@ -71,7 +70,7 @@ class MsgSend extends Command {
         data: encryptedMsg
       }
 
-      const result = await write.postEntry(data, appId)
+      const result = await this.write.postEntry(data, appId)
       console.log(`Data about P2WDB write: ${JSON.stringify(result, null, 2)}`)
 
       const hash = result.hash
@@ -88,31 +87,25 @@ class MsgSend extends Command {
       console.log(`Transaction ID : ${JSON.stringify(txidStr, null, 2)}`)
       return txidStr
     } catch (error) {
-      console.log('Error in msgSend()', error.message)
+      console.log('Error in msgSend()', error)
       throw error
     }
   }
 
-  // Encrypt using eccrypto
+  // Encrypt using encryptLib
   async encryptMsg (pubKey, msg) {
     try {
-      // Encrypt the message with the public key.
-      const pubKeyBuf = Buffer.from(pubKey, 'hex')
-      const bufferedMessage = Buffer.from(msg, 'hex')
+      if (!pubKey || typeof pubKey !== 'string') {
+        throw new Error('pubKey must be a string')
+      }
+      if (!msg || typeof msg !== 'string') {
+        throw new Error('msg must be a string')
+      }
+      const buff = Buffer.from(msg)
+      const hex = buff.toString('hex')
 
-      const structuredEj = await this.eccrypto.encrypt(pubKeyBuf, bufferedMessage)
-      // console.log(`structuredEj: ${JSON.stringify(structuredEj, null, 2)}`)
-
-      // Serialize the encrypted data object
-      const encryptedEj = Buffer.concat([
-        structuredEj.ephemPublicKey,
-        structuredEj.iv,
-        structuredEj.ciphertext,
-        structuredEj.mac
-      ])
-
-      const encryptedStr = encryptedEj.toString('hex')
-
+      const encryptedStr = await this.encryptLib.encryption.encryptFile(pubKey, hex)
+      // console.log(`encryptedStr: ${JSON.stringify(encryptedStr, null, 2)}`)
       return encryptedStr
     } catch (error) {
       console.log('Error in encryptMsg()', error)
@@ -120,22 +113,20 @@ class MsgSend extends Command {
     }
   }
 
-  // Encrypt using encryptLib
-  async bchEncryptMsg (pubKey, msg) {
-    try {
-      const buff = Buffer.from(msg)
-      const hex = buff.toString('hex')
-
-      const encryptedStr = await this.encryptLib.encryption.encryptFile(pubKey, hex)
-      console.log(`encryptedStr: ${JSON.stringify(encryptedStr, null, 2)}`)
-    } catch (error) {
-      console.log('Error in bchEncryptMsg()', error)
-      throw error
-    }
-  }
-
   async signalMessage (privateKey, hash, bchAddress, subject) {
     try {
+      if (!privateKey || typeof privateKey !== 'string') {
+        throw new Error('privateKey must be a string')
+      }
+      if (!hash || typeof hash !== 'string') {
+        throw new Error('hash must be a string')
+      }
+      if (!bchAddress || typeof bchAddress !== 'string') {
+        throw new Error('bchAddress must be a string')
+      }
+      if (!subject || typeof subject !== 'string') {
+        throw new Error('subject must be a string')
+      }
       const txHex = await this.messagesLib.memo.writeMsgSignal(
         privateKey,
         hash,
